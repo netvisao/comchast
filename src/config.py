7b383e08e4e0fedcc332ae3c11a689316fc1aeac
mea <00001@protonmail.ch>
2016-02-05 18:03:42 -0500

    Phrases ...

--- src/config.py
+++ src/config.py
@@ -1,27 +1,24 @@
 
 class Config(object):
-  Log_filename = 'logs/comchast.log'
-
-  ## TODO  
-  Twitter_key = ''
-  Twitter_secret = ''
-  Twitter_access_key = ''
-  Twitter_access_secret = ''
-  ## TODO - End
-
+  Log_filename = '/home/mea/comchast/logs/comchast.log'
+  Twitter_key = 'gsmgYNxwXVq9YHgarF09zUG4a'
+  Twitter_secret = 'zRLAAABJluNmKvfag10geRcmYzPE2nOFdhJMOsDN6WjexUu0FW' 
+  Twitter_access_key = '18223446-hyHyOFDY8pzk0Wd7AuwplChu74BITCoZslVg4HhIr'
+  Twitter_access_secret = 'DtQrDeFYRvKp6Dfm8bp9gzIgGR5zTMtgkIDeHSyLDVhWb'
   Comcast_DL_Threshold = 105 * .5
   Comcast_UPL_Threshold = 20 * .5
 
   Tweet_Templates = [
-    "@comcastcares please !! %d mbps when I pay for 105 ?",
-    "How come I am getting only %d mbps download when I am paying for 105 ? @comcastcares",
-    "Not Cool @comcastcares ! I paid for 105 mbps not %d !",
-    "Oh lordy ! %d mbps is NOT 105 @comcastcares", 
+    "@comcastcares wants me to take the banana.. %mbps instead of 105 - https://youtu.be/6n5uhr85Zmo",
+    "@comcastcares please !! %dmbps when I pay for 105?",
+    "How come I am getting only %dmbps download when I am paying for 105? @comcastcares",
+    "Not Cool @comcastcares ! I paid for 105 mbps not %d!",
+    "Oh lordy! %d mbps is NOT 105 @comcastcares", 
     "Martha! Martha! Martha! getting %d instead of 105mbps @comcastcares",
     "@comcastcares can you get your numbers in order? getting %d instead of 105mbps smh",
     "Looks like 105mbps is a dream not coming thru, reality is %dmbps - @comcastcares",
     "@comcastcares - why trolololo me with %dmpbs instead of the 105 I am paying for ?",
-    "@comcastcares - 105mbps looks high in the sky of the %dmbps land !",
+    "@comcastcares - 105mbps looks high in the sky of the %dmbps land!",
     "@comcastcares - Speeds look pale at %dmbps compared to the 105mbps I am paying for !",
     "@comcastcares - nightflight to venus would be a very, very long trip at %dmbps instead of the 105mbps I am billed for! - https://youtu.be/P-7-ENyE5yU"
  ]
